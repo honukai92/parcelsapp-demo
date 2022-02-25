7008f1dfd25809aace9124d5d8eb6a413e100117
honukai92 <honukai92@gmail.com>
2022-02-24 18:45:39 -0800

    update - selenium

--- app.py
+++ app.py
@@ -15,18 +15,17 @@ def favicon():
 @app.route('/')
 @app.route('/home')
 def home():
-    #op = webdriver.ChromeOptions()
-    #op.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-    #op.add_argument('--headless')
-    #op.add_argument('--no-sandbox')
-    #op.add_argument('--disable-dev-sh-usage')
+    op = webdriver.ChromeOptions()
+    op.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+    op.add_argument('--headless')
+    op.add_argument('--no-sandbox')
+    op.add_argument('--disable-dev-sh-usage')
 
-    #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
-    #driver.get('https://parcelsapp.com/en/tracking/9400128206335591615282')
-    #tracking_number = driver.find_element_by_xpath('//*[@id="tracking-info"]/div[1]/div[2]/div[2]/table/tbody/tr[1]/td[2]/span')
-    #driver.close()
-    return "hello world"
-
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
+    driver.get('https://parcelsapp.com/en/tracking/9400128206335591615282')
+    tracking_number = driver.find_element_by_xpath('//*[@id="tracking-info"]/div[1]/div[2]/div[2]/table/tbody/tr[1]/td[2]/span')
+    driver.close()
+    return tracking_number
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
     app.debug = True
